24a31ac5cad43d206d7c7c212f0a378b4f3fc78d
VasilKalchev <usraccs@gmail.com>
2016-08-06 21:26:56 +0300

    Increased the default number of maximul lines to 12.

--- src/LiquidMenu_config.h
+++ src/LiquidMenu_config.h
@@ -12,7 +12,7 @@ used in the library, also configures the messages output.
 const uint8_t MAX_FUNCTIONS = 8; ///< @note Default: 8
 
 /// Configures the number of available lines per screen.
-const uint8_t MAX_LINES = 8; ///< @note Default: 8
+const uint8_t MAX_LINES = 12; ///< @note Default: 12
 
 /// Configures the number of available screens per menu.
 const uint8_t MAX_SCREENS = 16; ///< @note Default: 16
@@ -20,5 +20,5 @@ const uint8_t MAX_SCREENS = 16; ///< @note Default: 16
 /// Configures the number of available manus per menus system.
 const uint8_t MAX_MENUS = 12; ///< @note Defaul: 12
 
-// Turns the debuggin messages on or off.
+// Turns the debugging messages on or off.
 #define DEBUGGING_MESSAGES false ///< @note Default: false
