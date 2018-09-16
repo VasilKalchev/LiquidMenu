1f951430accd000d80f8ca21679259199fe3bbf0
Vasil Kalchev <vase7u@gmail.com>
2018-09-17 01:02:42 +0300

    doxygen update

--- doc/Doxygen/html/menudata.js
+++ doc/Doxygen/html/menudata.js
@@ -1,42 +1,65 @@
+/*
+@ @licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2017 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
 var menudata={children:[
-{text:'Main Page',url:'index.html'},
-{text:'Related Pages',url:'pages.html'},
-{text:'Classes',url:'annotated.html',children:[
-{text:'Class List',url:'annotated.html'},
-{text:'Class Members',url:'functions.html',children:[
-{text:'All',url:'functions.html',children:[
-{text:'a',url:'functions.html#index_a'},
-{text:'c',url:'functions.html#index_c'},
-{text:'h',url:'functions.html#index_h'},
-{text:'i',url:'functions.html#index_i'},
-{text:'l',url:'functions.html#index_l'},
-{text:'n',url:'functions.html#index_n'},
-{text:'o',url:'functions.html#index_o'},
-{text:'p',url:'functions.html#index_p'},
-{text:'s',url:'functions.html#index_s'},
-{text:'u',url:'functions.html#index_u'}]},
-{text:'Functions',url:'functions_func.html',children:[
-{text:'a',url:'functions_func.html#index_a'},
-{text:'c',url:'functions_func.html#index_c'},
-{text:'h',url:'functions_func.html#index_h'},
-{text:'i',url:'functions_func.html#index_i'},
-{text:'l',url:'functions_func.html#index_l'},
-{text:'n',url:'functions_func.html#index_n'},
-{text:'o',url:'functions_func.html#index_o'},
-{text:'p',url:'functions_func.html#index_p'},
-{text:'s',url:'functions_func.html#index_s'},
-{text:'u',url:'functions_func.html#index_u'}]}]}]},
-{text:'Files',url:'files.html',children:[
-{text:'File List',url:'files.html'},
-{text:'File Members',url:'globals.html',children:[
-{text:'All',url:'globals.html',children:[
-{text:'d',url:'globals.html#index_d'},
-{text:'i',url:'globals.html#index_i'},
-{text:'l',url:'globals.html#index_l'},
-{text:'m',url:'globals.html#index_m'},
-{text:'p',url:'globals.html#index_p'},
-{text:'r',url:'globals.html#index_r'}]},
-{text:'Functions',url:'globals_func.html'},
-{text:'Variables',url:'globals_vars.html'},
-{text:'Enumerations',url:'globals_enum.html'},
-{text:'Macros',url:'globals_defs.html'}]}]}]}
+{text:"Main Page",url:"index.html"},
+{text:"Related Pages",url:"pages.html"},
+{text:"Classes",url:"annotated.html",children:[
+{text:"Class List",url:"annotated.html"},
+{text:"Class Members",url:"functions.html",children:[
+{text:"All",url:"functions.html",children:[
+{text:"a",url:"functions.html#index_a"},
+{text:"c",url:"functions.html#index_c"},
+{text:"h",url:"functions.html#index_h"},
+{text:"i",url:"functions.html#index_i"},
+{text:"l",url:"functions.html#index_l"},
+{text:"n",url:"functions.html#index_n"},
+{text:"o",url:"functions.html#index_o"},
+{text:"p",url:"functions.html#index_p"},
+{text:"s",url:"functions.html#index_s"},
+{text:"u",url:"functions.html#index_u"}]},
+{text:"Functions",url:"functions_func.html",children:[
+{text:"a",url:"functions_func.html#index_a"},
+{text:"c",url:"functions_func.html#index_c"},
+{text:"h",url:"functions_func.html#index_h"},
+{text:"i",url:"functions_func.html#index_i"},
+{text:"l",url:"functions_func.html#index_l"},
+{text:"n",url:"functions_func.html#index_n"},
+{text:"o",url:"functions_func.html#index_o"},
+{text:"p",url:"functions_func.html#index_p"},
+{text:"s",url:"functions_func.html#index_s"},
+{text:"u",url:"functions_func.html#index_u"}]}]}]},
+{text:"Files",url:"files.html",children:[
+{text:"File List",url:"files.html"},
+{text:"File Members",url:"globals.html",children:[
+{text:"All",url:"globals.html",children:[
+{text:"d",url:"globals.html#index_d"},
+{text:"i",url:"globals.html#index_i"},
+{text:"l",url:"globals.html#index_l"},
+{text:"m",url:"globals.html#index_m"},
+{text:"p",url:"globals.html#index_p"},
+{text:"r",url:"globals.html#index_r"}]},
+{text:"Functions",url:"globals_func.html"},
+{text:"Variables",url:"globals_vars.html"},
+{text:"Enumerations",url:"globals_enum.html"},
+{text:"Macros",url:"globals_defs.html"}]}]}]}
