50b1f232531ec6494f7a9d94b5c74951eb172a59
Jose Manuel <jmpmscorp@hotmail.com>
2018-10-05 09:15:48 +0200

    Added functions as parameters, selective update, get current screen ref

--- src/recognizeType.cpp
+++ src/recognizeType.cpp
@@ -65,3 +65,84 @@ DataType recognizeType(double variable) {
 	return DataType::DOUBLE;
 	variable = variable;
 }
+
+
+DataType recognizeType(boolFnPtr variable)
+{
+	return DataType::BOOL_RET_FUNC;
+	variable = variable;
+}
+
+
+DataType recognizeType(int8tFnPtr variable)
+{
+	return DataType::INT8_T_RET_FUNC;
+	variable = variable;
+}
+
+
+DataType recognizeType(uint8tFnPtr variable)
+{
+	return DataType::UINT8_T_RET_FUNC;
+	variable = variable;
+}
+
+
+DataType recognizeType(int16tFnPtr variable)
+{
+	return DataType::INT16_T_RET_FUNC;
+	variable = variable;
+}
+
+DataType recognizeType(uint16tFnPtr variable)
+{
+	return DataType::UINT8_T_RET_FUNC;
+	variable = variable;
+}
+
+
+DataType recognizeType(int32tFnPtr variable)
+{
+	return DataType::INT32_T_RET_FUNC;
+	variable = variable;
+}
+
+
+DataType recognizeType(uint32tFnPtr variable)
+{
+	return DataType::UINT32_T_RET_FUNC;
+	variable = variable;
+}
+
+
+DataType recognizeType(floatFnPtr variable)
+{
+	return DataType::FLOAT_RET_FUNC;
+	variable = variable;
+}
+
+DataType recognizeType(doubleFnPtr variable)
+{
+	return DataType::DOUBLE_RET_FUNC;
+	variable = variable;
+}
+
+DataType recognizeType(charFnPtr variable)
+{
+	return DataType::CHAR_RET_FUNC;
+	variable = variable;
+}
+
+
+DataType recognizeType(charPtrFnPtr variable)
+{
+	return DataType::CHAR_PTR_RET_FUNC;
+	variable = variable;
+}
+
+
+DataType recognizeType(constcharPtrFnPtr variable)
+{
+	return DataType::CONST_CHAR_PTR_RET_FUNC;
+	variable = variable;
+}
